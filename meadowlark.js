2c73abfe99953ba8b3fa82512b008aa6aaaf8f86
Seun Kujebe <kujebeseun@gmail.com>
2014-09-07 13:24:46 +0000

    Commit

--- meadowlark.js
+++ meadowlark.js
@@ -4,16 +4,16 @@ var app = express();
 var port = (process.env.PORT || 3000);
 // custom 404 page
 app.use(function(req, res){
-res.type('text/plain');
-res.status(404);
-res.send('404 - Not Found');
+    res.type('text/plain');
+    res.status(404);
+    res.send('Error 404 - Not Found');
 });
 // custom 500 page
 app.use(function(err, req, res, next){
-console.error(err.stack);
-res.type('text/plain');
-res.status(500);
-res.send('500 - Server Error');
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
 });
 app.listen(port, function(){
 console.log( 'Express started on http://localhost:' + port + '; press Ctrl-C to terminate.' );
