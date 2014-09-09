62d92300a16b86363f11e6cdba0157072c9347fb
Seun Kujebe <kujebeseun@gmail.com>
2014-09-09 10:12:17 +0000

    Added View Files and Handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
-//app.set('port', process.env.PORT || 3000);
-var port = (process.env.PORT || 3000);
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
+
 // custom 404 page
 app.use(function(req, res){
     res.type('text/plain');
