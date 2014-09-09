e646bc7fead1b60b47e35620cb8bcb483bb8dd81
Seun Kujebe <kujebeseun@gmail.com>
2014-09-09 17:26:03 +0000

    Custom module

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var fortune = require('./lib/fortune.js');
 var port = process.env.PORT || 3000;
 
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
-
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
 
 //Middlewares
 app.use(express.static(__dirname + '/public'));
@@ -25,9 +18,7 @@ app.get('/',function(req, res){
 
 //About Us Page
 app.get('/about', function(req, res){
-   var randomFortune =
-    fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+      res.render('about', { fortune: fortune.getFortune() });
 });
 
 //Custom 404 page
